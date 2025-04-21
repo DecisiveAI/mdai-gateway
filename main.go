fcc9f793a24f8de60462d96a74da86361c3b384b
Joe Churchman <joe@mydecisive.ai>
2025-04-21 14:27:12 -0700

    organizing, comments

--- main.go
+++ main.go
@@ -220,9 +220,6 @@ func adaptPrometheusAlertToMdaiEvents(payload types.AlertManagerPayload) []types
 
 func handleAlertsPost() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		processMutex.Lock()
-		defer processMutex.Unlock()
-
 		if r.Method != http.MethodPost {
 			http.Error(w, "Only POST method is supported", http.StatusMethodNotAllowed)
 			return
@@ -254,9 +251,6 @@ func handleAlertsPost() http.HandlerFunc {
 
 func handleEventsPost() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		processMutex.Lock()
-		defer processMutex.Unlock()
-
 		if r.Method != http.MethodPost {
 			http.Error(w, "Only POST method is supported", http.StatusMethodNotAllowed)
 			return
