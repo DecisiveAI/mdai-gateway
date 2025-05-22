64feded62e4d70dd3c7ef2c6f61b9758122361dd
dmitry <dmitry@mydecisive.ai>
2025-05-22 17:53:29 -0400

    set api, beginning

--- variables.go
+++ variables.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/go-logr/zapr"
 	"github.com/valkey-io/valkey-go"
@@ -19,8 +22,17 @@ import (
 
 const manualEnvConfigMapNamePostfix = "-manual-variables"
 
+//type VariableType string
+//
+//const SET = VariableType("set")
+//const MAP = VariableType("map")
+//const STRING = VariableType("string")
+//const INT = VariableType("int")
+//const BOOL = VariableType("boolean")
+
 // getConfiguredManualVariables  returns a map of Hub names to their corresponding ManualVariables:Types
 func getConfiguredManualVariables(ctx context.Context, k8sClient dynamic.Interface) (map[string]any, error) {
+	// TODO: make ConfiMap fetcher async
 	gvrCR := schema.GroupVersionResource{
 		Group:    "hub.mydecisive.ai",
 		Version:  "v1",
@@ -145,9 +157,110 @@ func HandleGetVariables(ctx context.Context, valkeyClient valkey.Client, k8sClie
 	}
 }
 
+func HandleSetVariables(ctx context.Context, k8sClient dynamic.Interface) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var response any
+
+		if !headersOk(r) {
+			WriteJSONResponse(w, http.StatusBadRequest, "Wrong Content-Type header")
+			return
+		}
+
+		hubsVariables, err := getConfiguredManualVariables(ctx, k8sClient)
+		if err != nil && hubsVariables == nil {
+			WriteJSONResponse(w, http.StatusInternalServerError, response)
+			return
+		}
+		hubName := r.PathValue("hubName")
+		varName := r.PathValue("varName")
+		if hubName == "" || varName == "" {
+			WriteJSONResponse(w, http.StatusBadRequest, "Missing hub or variable name")
+			return
+		}
+
+		hub := hubsVariables[hubName]
+		if hub == nil {
+			WriteJSONResponse(w, http.StatusNotFound, "Hub not found")
+			return
+		}
+
+		varType, ok := hub.(map[string]string)[varName]
+		if !ok {
+			WriteJSONResponse(w, http.StatusNotFound, "Variable not found")
+			return
+		}
+
+		var raw map[string]json.RawMessage
+		if err := json.NewDecoder(r.Body).Decode(&raw); err != nil {
+			http.Error(w, "Invalid request payload. Not map[string]json.RawMessage", http.StatusBadRequest)
+			return
+		}
+
+		var payload any
+		var eventPayload MdaiEvent
+		switch varType {
+		case "set":
+			{
+				var payloadSet []string
+				if err := json.Unmarshal(raw["value"], &payloadSet); err != nil {
+					http.Error(w, "Invalid request payload. String list expected", http.StatusBadRequest)
+					return
+				}
+				payload = payloadSet
+				eventPayload = newEventPayload(hubName, varName, varType, "set", payload)
+			}
+		case "map":
+			{
+				var payloadMap map[string]string
+				if err := json.Unmarshal(raw["value"], &payloadMap); err != nil {
+					http.Error(w, "Invalid request payload. Map expected", http.StatusBadRequest)
+					return
+				}
+				payload = payloadMap
+				eventPayload = newEventPayload(hubName, varName, varType, "set", payload)
+			}
+		case "string", "int", "boolean":
+			{
+				var payloadString string
+				if err := json.Unmarshal(raw["value"], &payloadString); err != nil {
+					http.Error(w, "Invalid request payload. String expected", http.StatusBadRequest)
+					return
+				}
+				payload = payloadString
+				eventPayload = newEventPayload(hubName, varName, varType, "set", payload)
+			}
+		}
+		logger.Info("POST:", zap.Any("payload", payload))
+		WriteJSONResponse(w, http.StatusOK, eventPayload)
+
+	}
+}
+
+func newEventPayload(hubName string, varName string, varType string, action string, payload any) MdaiEvent {
+	return MdaiEvent{
+		Id:        CreateEventUuid(),
+		Name:      strings.Join([]string{"static_variable", action, hubName, varName}, "__"),
+		HubName:   hubName,
+		Timestamp: time.Now(),
+		Source:    "static_variable_api",
+		Payload: fmt.Sprintf(
+			"{hub: %s, variable: %s, type: %s, operation: %s, value: %s}",
+			hubName,
+			varName,
+			varType,
+			action,
+			payload),
+	}
+
+}
+
 func WriteJSONResponse(w http.ResponseWriter, status int, v any) error {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
 
 	return json.NewEncoder(w).Encode(v)
+}
+
+func headersOk(r *http.Request) bool {
+	return r.Header.Get("Content-Type") == "application/json"
 }
