d647b2e2581815db8af12e43ffce76e10bd96d02
Trent Vigar <trent@mydecisive.ai>
2026-03-11 16:10:19 -0400

    ENG-1107 updating secret name to match what we're expecting in the deployment values

--- internal/integration/datadog.go
+++ internal/integration/datadog.go
@@ -11,12 +11,6 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
-const (
-	DataDogIntegrationType Type = "datadog"
-
-	dataDogIntegrationSecretName string = "mdai-datadog-integration" // nolint: gosec
-)
-
 type DataDogIntegrationData struct {
 	APIKey string `json:"api_key"`
 	DDUrl  string `json:"dd_url"`
@@ -28,14 +22,14 @@ type DataDogIntegration struct {
 
 var _ Integration[DataDogIntegrationData] = (*DataDogIntegration)(nil)
 
-// GetIntegrations retrieves any existing integrations in the provided namespace for the "mdai-datadog-integration" secret.
+// GetIntegrations retrieves any existing integrations in the provided namespace for the "mdai-gateway-integration" secret.
 func (ddi *DataDogIntegration) GetIntegrations(ctx context.Context, namespace string) (map[string]DataDogIntegrationData, error) {
-	secret, err := ddi.K8sClient.CoreV1().Secrets(namespace).Get(ctx, dataDogIntegrationSecretName, metav1.GetOptions{})
+	secret, err := ddi.K8sClient.CoreV1().Secrets(namespace).Get(ctx, integrationSecretName, metav1.GetOptions{})
 	if err != nil {
 		if k8serrors.IsNotFound(err) {
 			return nil, nil // nolint: nilnil
 		}
-		return nil, fmt.Errorf("failed to get secret %s: %w", dataDogIntegrationSecretName, err)
+		return nil, fmt.Errorf("failed to get secret %s: %w", integrationSecretName, err)
 	}
 
 	integrations := make(map[string]DataDogIntegrationData)
@@ -50,35 +44,35 @@ func (ddi *DataDogIntegration) GetIntegrations(ctx context.Context, namespace st
 	return integrations, nil
 }
 
-// SetIntegration adds or updates the "mdai-datadog-integration" secret for the provided namespace.
+// SetIntegration adds or updates the "mdai-gateway-integration" secret for the provided namespace.
 func (ddi *DataDogIntegration) SetIntegration(ctx context.Context, namespace, integrationName string, integrationData DataDogIntegrationData) error {
 	jsonData, err := json.Marshal(integrationData)
 	if err != nil {
 		return fmt.Errorf("failed to marshal integration data: %w", err)
 	}
 
-	secret, err := ddi.K8sClient.CoreV1().Secrets(namespace).Get(ctx, dataDogIntegrationSecretName, metav1.GetOptions{})
+	secret, err := ddi.K8sClient.CoreV1().Secrets(namespace).Get(ctx, integrationSecretName, metav1.GetOptions{})
 	isNotFound := k8serrors.IsNotFound(err)
 	if err != nil && !isNotFound {
-		return fmt.Errorf("failed to fetch secret %s: %w", dataDogIntegrationSecretName, err)
+		return fmt.Errorf("failed to fetch secret %s: %w", integrationSecretName, err)
 	}
 
 	if isNotFound {
 		// Create the secret if it does not exist
-		return createIntegrationSecret(ctx, ddi.K8sClient, namespace, dataDogIntegrationSecretName, integrationName, jsonData)
+		return createIntegrationSecret(ctx, ddi.K8sClient, namespace, integrationSecretName, integrationName, jsonData)
 	}
 	// Update the secret if it already exists
 	return updateSecretWithIntegration(ctx, ddi.K8sClient, namespace, secret, integrationName, jsonData)
 }
 
-// DeleteIntegration removes a named integration from the "mdai-datadog-integration" secret in the provided namespace.
+// DeleteIntegration removes a named integration from the "mdai-gateway-integration" secret in the provided namespace.
 func (ddi *DataDogIntegration) DeleteIntegration(ctx context.Context, namespace, integrationName string) error {
-	secret, err := ddi.K8sClient.CoreV1().Secrets(namespace).Get(ctx, dataDogIntegrationSecretName, metav1.GetOptions{})
+	secret, err := ddi.K8sClient.CoreV1().Secrets(namespace).Get(ctx, integrationSecretName, metav1.GetOptions{})
 	if err != nil {
 		if k8serrors.IsNotFound(err) {
 			return nil
 		}
-		return fmt.Errorf("failed to fetch secret %s: %w", dataDogIntegrationSecretName, err)
+		return fmt.Errorf("failed to fetch secret %s: %w", integrationSecretName, err)
 	}
 
 	if secret.Data == nil {
@@ -92,7 +86,7 @@ func (ddi *DataDogIntegration) DeleteIntegration(ctx context.Context, namespace,
 
 	_, err = ddi.K8sClient.CoreV1().Secrets(namespace).Update(ctx, secret, metav1.UpdateOptions{})
 	if err != nil {
-		return fmt.Errorf("failed to update secret %s after deletion: %w", dataDogIntegrationSecretName, err)
+		return fmt.Errorf("failed to update secret %s after deletion: %w", integrationSecretName, err)
 	}
 
 	return nil
