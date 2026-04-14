e268d687ead314ab2b9658d987700dd767a84b63
Robin Law <Robin-Law@users.noreply.github.com>
2026-04-14 17:01:39 -0500

    add _total to exporter metric queries

--- internal/metrics/connection_status.go
+++ internal/metrics/connection_status.go
@@ -102,19 +102,19 @@ func (cs *ConnectionStatus) IsTelemetryFlowing(ctx context.Context, connectionNa
 			promQuery = lo.Ternary(
 				ie == Ingress,
 				fmt.Sprintf("otelcol_receiver_accepted_log_records_total{receiver=%q, mdai_connection=%q}", "datadog", connectionName),
-				fmt.Sprintf("otelcol_exporter_sent_log_records{exporter=%q, mdai_connection=%q}", "datadog", connectionName),
+				fmt.Sprintf("otelcol_exporter_sent_log_records_total{exporter=%q, mdai_connection=%q}", "datadog", connectionName),
 			)
 		case telemetry.Traces:
 			promQuery = lo.Ternary(
 				ie == Ingress,
 				fmt.Sprintf("otelcol_receiver_accepted_spans_total{receiver=%q, mdai_connection=%q}", "datadog", connectionName),
-				fmt.Sprintf("otelcol_exporter_sent_spans{exporter=%q, mdai_connection=%q}", "datadog", connectionName),
+				fmt.Sprintf("otelcol_exporter_sent_spans_total{exporter=%q, mdai_connection=%q}", "datadog", connectionName),
 			)
 		case telemetry.Metrics:
 			promQuery = lo.Ternary(
 				ie == Ingress,
 				fmt.Sprintf("otelcol_receiver_accepted_metric_points_total{receiver=%q, mdai_connection=%q}", "datadog", connectionName),
-				fmt.Sprintf("otelcol_exporter_sent_metric_points{exporter=%q, mdai_connection=%q}", "datadog", connectionName),
+				fmt.Sprintf("otelcol_exporter_sent_metric_points_total{exporter=%q, mdai_connection=%q}", "datadog", connectionName),
 			)
 		default:
 			return false, fmt.Errorf("unknown telemetry type: %s", connectionType)
