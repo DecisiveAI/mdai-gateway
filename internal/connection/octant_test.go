ffb003698d610fd7d7dd5625009a87607f8e64ca
Robin Law <Robin-Law@users.noreply.github.com>
2026-04-17 13:03:04 -0500

    the linter hated that suggestion

--- internal/connection/octant_test.go
+++ internal/connection/octant_test.go
@@ -539,7 +539,7 @@ func TestGetConnectionStatus_Success(t *testing.T) {
 
 	promServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		bodyBytes, _ := io.ReadAll(r.Body)
-		defer r.Body.Close() //nolint: errcheck
+		defer r.Body.Close() // nolint: errcheck
 		bodyString := string(bodyBytes)
 
 		responseString := `{"status":"success","data":{"resultType":"vector","result":[{"metric":{},"value":[1712419691,"5"]}]}}`
